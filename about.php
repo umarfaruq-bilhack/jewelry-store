4a01786c587d9aafae5493c217580afc2310bbfd
umarfaruq-bilhack <59941240+umarfaruq-bilhack@users.noreply.github.com>
2025-10-16 11:51:33 +0000

    Added about page

--- about.php
+++ about.php
@@ -1,8 +1,4 @@
-<?php
-include 'header.php';
-?>
+<?php include 'header.php'; ?>
 <h1>About Our Jewelry Store</h1>
-<p>We sell luxury jewelry with timeless beauty.</p>
-<?php
-include 'footer.php';
-?>
+<p>We sell premium jewelry crafted with passion.</p>
+<?php include 'footer.php'; ?>
