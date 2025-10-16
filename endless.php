7beacb3552753ed406c2e5b150033bbd93357ff3
umarfaruq-bilhack <59941240+umarfaruq-bilhack@users.noreply.github.com>
2025-10-16 12:18:01 +0000

    Added endless page

--- endless.php
+++ endless.php
@@ -9,5 +9,10 @@
 Endless Web3 Genesis Cloud</h1>
 <h3>Overview</h3>
 <p>The Endless Web3 Genesis Cloud is a revolutionary platform designed to empower developers and businesses to build, deploy, and scale decentralized applications (dApps) with ease. Our cloud infrastructure leverages the power of blockchain technology to provide a secure, scalable, and efficient environment for dApp development.</p>
+<h4>Core Value</h4>
+<p>The Endless was founded with the vision of leveraging technological innovation to empower users with greater autonomy, ensuring data privacy and security. It aims to foster a decentralized co-creation ecosystem where users can create and fully enjoy the value they deserve in the digital world. By shifting the focus of Web3 from financial orientation to practical utility, the Endless seeks to deliver on the promise of decentralized technologies by enabling user empowerment and collaborative value creation.
+
+The core values of Endless include:</p>
+
 </body>
 </html>
