309e376e09a0877584628f8d2ed33555f6e00e09
umarfaruq-bilhack <59941240+umarfaruq-bilhack@users.noreply.github.com>
2025-10-16 12:07:14 +0000

    Added contact page

--- contact.php
+++ contact.php
@@ -1,4 +1,6 @@
 <?php include 'header.php'; ?>
 <h1>Contact Jewelry Store</h1>
 <p>We sell premium jewelry crafted with passion.</p>
+<h1>We're open 24/7</h1>
+<p>We sell premium jewelry crafted with passion.</p>
 <?php include 'footer.php'; ?>
